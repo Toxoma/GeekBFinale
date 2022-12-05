da7a92fd5276324b332f5380636144a8084aa72c
D.Veselov <d.veselov@techmoney.ru>
2022-12-05 17:55:34 +0300

    programm

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string [] mas = new string[] { "Tom", "Sam", "Bo", "Kate", "Toa", "Alice", "2", ":-)" };
+
+string [] Rabota(string [] array)
+{
+    string [] result = new string[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result = result.Append(array[i]).ToArray();
+        }
+    }
+    return result;
+}
+
+void Vivod<T>(T[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{i}) {array[i]}");
+    }
+}
+
+string [] result = Rabota(mas);
+Vivod(result);
